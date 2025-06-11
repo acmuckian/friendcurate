092422d42428c074a16c85e20830c307ae975386
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-06-11 12:52:32 +0100

    added summernote

--- img/models.py
+++ img/models.py
@@ -3,13 +3,25 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+# class Category(models.Model):
+#     name = models.CharField(max_length=50)
+
+
 class Img(models.Model):
+    options = ( ( 'draft', 'Draft')
+               ('published', 'Published')
+    )
     image = models.ImageField()
     title = models.CharField(max_length=200, unique=True)
+    caption = models.CharField(max_length=1000, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="img_posts"
     )
+    favourites = models.ManyToManyField(
+        User, related_name='favourite', default=None, blank=True)
+    status = models.CharField(max_length=10, choices=options, default='draft')
     created_on = models.DateTimeField(auto_now_add=True)
+    
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
