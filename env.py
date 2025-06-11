d4746882fb56f3f579d312bfca880383638ae263
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-06-11 11:06:52 +0100

    ready for heroku deployment

---
+++ env.py
@@ -0,0 +1,6 @@
+
+
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_OeihsVM4qg2t@ep-summer-waterfall-a2uz9dto.eu-central-1.aws.neon.tech/mumbo_slick_shun_384120")
---
+++ img/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Img(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="img_posts"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Img, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+        
