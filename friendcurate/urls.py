2a0eedc2daa18ddc1676e677274b627a9f687aab
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-06-12 15:16:02 +0100

    modified login and logout

--- friendcurate/urls.py
+++ friendcurate/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("img.urls"), name="img-urls"),
+    path("accounts/", include("allauth.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
