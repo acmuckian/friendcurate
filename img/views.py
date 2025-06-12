a8f65ec59d0b64a8940daa3a8c5e3370e3246a95
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-06-12 14:43:36 +0100

    added allauth

--- img/views.py
+++ img/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Img, Comment
+from .forms import CommentForm
 # Create your views here.
 class ImgList(generic.ListView):
     model = Img
@@ -13,6 +14,15 @@ def img_detail(request, slug):
     queryset = Img.objects.filter(status='published')
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.filter(approved=True).order_by('created_on')
+    comment_form = CommentForm()
+    if request.method == "POST":
+        comment_form = comment_form(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post 
+            comment.save()
+    
 
     return render(
         request,
@@ -20,6 +30,7 @@ def img_detail(request, slug):
         {
             "img": post,
             "comments": comments,
+            "comment_form": comment_form,
         },
     )
 
