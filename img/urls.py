376fc89ca36d34492401651d1f6276e2f3600a70
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-06-14 20:37:03 +0100

    added favourites feature

--- img/urls.py
+++ img/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ImgList.as_view(), name='home'),
-    path('<slug:slug>/', views.img_detail, name="img_detail")
+    path('fav/<int:id>/', views.add_favourite, name='add_favourite'),
+    path('favourites/', views.my_favourites, name="my_favourites"),
+    path('<slug:slug>/', views.img_detail, name="img_detail"),
 ]
