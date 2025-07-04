236c7ea24bdefd85c56cf7502e8cbaad7ddad956
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-07-04 20:48:26 +0100

    adding delete/confirm functionality on images and comments

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Friendcurate 
+
+![Friendcurate on amiresponsive.dev](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Friendcurate")
+
+## Features 
+
+### Home Page 
+
+### Image Display Page 
+
+### Login Page 
+
+### Logout Page 
+
 ## UX 
 
 ### Wireframes 
@@ -6,5 +19,16 @@
 https://wireframe.cc/5UDuvW
 
 ### User Stories
+
+1. As a visitor to the site, I want to be able to create an account so I can comment on images, edit my comments and add images myself.
+
+2. As a user, I want to be able to add images that other users can comment on and add to their favourites. 
+
+3. As an admin, I want to be able to check images and captions, comments to make sure they are okay and up to date and approved. 
+
+4. As a user, I want to be able to add images to my favourites collection and to be able to view my collection. 
+
+5. 
+
 
 ### Agile Development
--- img/views.py
+++ img/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from .models import Img, Comment
 from .forms import CommentForm
-from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 class ImgList(generic.ListView):
     model = Img
@@ -35,13 +38,85 @@ def img_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+def img_delete(request, slug):
+    queryset = Img.objects.filter(status="published")
+    img = get_object_or_404(queryset, slug=slug)
+    if img.author == request.user:
+        img.delete()
+        messages.add_message(request, messages.SUCCESS, 'Image removed!')
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own images!')
+        return redirect('img_detail', slug=img.slug)
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+    """
+    if request.method == "POST":
+
+        queryset = Img.objects.filter(status='published')
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+    return HttpResponseRedirect(reverse('img_detail', args=[slug]))
+
+@login_required
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual comment.
+
+    **Context**
+
+    ``img``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    """
+    queryset = Img.objects.filter(status="published")
+    img = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id, post=img)
+    if request.method == "POST":
+        if comment.author == request.user:
+            comment.delete()
+            messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+        else:
+         messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
+    else:
+        messages.error(request, 'Invalid request method.')
+    return HttpResponseRedirect(reverse('img_detail', args=[slug]))
+
 @login_required
 def add_favourite(request, id):
     img = get_object_or_404(Img, id=id)
     if img.favourites.filter(id=request.user.id).exists():
         img.favourites.remove(request.user)
+        messages.add_message(request, messages.SUCCESS, 'Favourite removed!')
     else:
         img.favourites.add(request.user)
+        messages.add_message(request, messages.SUCCESS, 'Favourite added!')
     return redirect('img_detail', slug=img.slug)
 
 @login_required
