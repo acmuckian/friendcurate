54e3f5a22d9e0cf3d36ce52cbc67f1e3312f3a30
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-07-16 18:53:29 +0100

    added profile and about

---
+++ about/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.about_site,
+         name='about'),
+]
--- friendcurate/urls.py
+++ friendcurate/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from img.views import ImgList
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("img.urls"), name="img-urls"),
