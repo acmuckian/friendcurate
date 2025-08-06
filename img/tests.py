cd9403e1757e8832febdbb4740eaacf0970fed05
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-08-06 21:13:12 +0100

    added changes to img detail

--- img/tests.py
+++ img/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import CommentForm, ProfileUpdateForm, UserUpdateForm
+from .forms import CommentForm, ProfileUpdateForm, UserUpdateForm, CreateImg
 # Create your tests here.
 
 class TestCommentForm(TestCase):
@@ -10,6 +10,15 @@ class TestCommentForm(TestCase):
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
         self.assertFalse(comment_form.is_valid(), msg="Form is valid")
+
+class TestImgForm(TestCase):
+    def img_form_is_valid(self):
+        img_form = CreateImg({
+            "title": "My Img",
+            "image": "img.png",
+            "caption": "My Caption"
+        })
+        self.assertTrue(img_form.is_valid)
 
 class TestProfileUpdateForm(TestCase):
 
