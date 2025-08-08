d98237b34150ae75ede272ba49639a250fb4393f
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-08-08 17:44:03 +0100

    changing newsletter form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import ContactRequest
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from .forms import ContactForm, NewsletterForm
+
 # Create your views here.
 
 def contact_us(request):
@@ -12,3 +14,26 @@ def contact_us(request):
         "contact_form": contact_form
         }
     )
+def subscribe(request):
+    if request.method == "POST":
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You subscribed to our newsletter!")
+            email = request.POST.get("email")
+            subject = "Thanks for subscribing to Friendcurate"
+            message = (
+                "Thank you for subscribing to our newsletter,"
+                + "you will see the latest developments in visual arts and about our site!"
+                + "Go back https://friendcurate.herokuapp.com/"
+            )
+            from_email = "tmarkec@gmail.com"
+            recipient_list = [email]
+            send_mail(subject,
+                      message, from_email, recipient_list, fail_silently=False)
+            return redirect("/")
+    else:
+        form = NewsletterForm
+
+    context = {"form": form}
+    return render(request, "index.html", context)
