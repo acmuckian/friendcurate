50cbafb978b3d16ce406b9d999b01c7fbdf62c52
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-08-11 11:22:06 +0100

    Update STATIC_URL to include leading slash for proper URL resolution

--- friendcurate/settings.py
+++ friendcurate/settings.py
@@ -172,7 +172,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_FINDERS = [
